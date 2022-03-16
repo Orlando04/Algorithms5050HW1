9a38d89510ee7f224af2f31321cec736d9a654bb
Orlando04 <orlando.medina4@upr.edu>
2022-03-16 16:38:14 -0400

    Create PartitionEvenOdd.cpp
    started writting pseudocode for the function

---
+++ PartitionEvenOdd.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+using namespace std;
+
+// initialize an array containing all elements to be partitioned
+// create the PartitionEvenOdd function
+// -> recieve the array
+/* -> run through it using iterators i and j 
+       if the spot in i % 2 == 0 we move j 
+       when j % 2 != 0 we swap with i */
