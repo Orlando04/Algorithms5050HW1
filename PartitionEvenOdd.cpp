8e26fc07d94121a6d057246d9069fc4b04ea58f5
Orlando04 <orlando.medina4@upr.edu>
2022-03-18 10:53:27 -0400

    working on the function

--- PartitionEvenOdd.cpp
+++ PartitionEvenOdd.cpp
@@ -7,3 +7,21 @@ using namespace std;
 /* -> run through it using iterators i and j 
        if the spot in i % 2 == 0 we move j 
        when j % 2 != 0 we swap with i */
+
+void PartitionEvenOdd(int A[]){
+       // Store the size of A
+       int SIZE = sizeof(A);
+
+       // we recieved the array
+       for (int i = 0; i < SIZE; i++){
+              cout << A[i] << endl;
+       }
+
+}
+
+int main(){
+      int partition_array[] = {7, 17, 74, 21, 7, 9, 26, 10};
+      PartitionEvenOdd(partition_array);
+
+      return 0;
+}
