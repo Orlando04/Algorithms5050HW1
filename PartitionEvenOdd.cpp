db9375612c2726494244264bdf89bad11eeaa6af
Orlando04 <orlando.medina4@upr.edu>
2022-03-21 22:19:26 -0400

    organized the pseudocode

--- PartitionEvenOdd.cpp
+++ PartitionEvenOdd.cpp
@@ -1,27 +1,41 @@
 #include <iostream> 
 using namespace std;
 
-// initialize an array containing all elements to be partitioned
-// create the PartitionEvenOdd function
-// -> recieve the array
-/* -> run through it using iterators i and j 
-       if the spot in i % 2 == 0 we move j 
-       when j % 2 != 0 we swap with i */
+// ---------- Pseudocode for PartitionEvenOdd ----------
+// 1. initialize an array containing all elements to be partitioned
+// 2. create the PartitionEvenOdd function
+// 3. store the size of the array
+// 4. use a for loop to run through the array using iterators (named i and j)
+// 5. if the spot i is an even number, increment j
+// 6. if it's not an even number, swap with j. Increment i & j   
 
 void PartitionEvenOdd(int A[]){
-       // Store the size of A
-       int SIZE = sizeof(A);
+  //Store the size of A
+  int SIZE = sizeof(A);
 
-       // we recieved the array
-       for (int i = 0; i < SIZE; i++){
-              cout << A[i] << endl;
-       }
+  // run through the array
+  for (int i = 0; i < SIZE; i++){
+    // second iterator to save the position
+    int j = 0;
+    
+    // check if the number in i is even or odd
+    if (A[i] % 2 == 0){
+      j++;
+    }
 
+    else {
+      swap(A[i],A[j]);
+      j++; i++;
+    }
+    
+    cout << A[i] << " "; 
+  }
 }
 
 int main(){
-      int partition_array[] = {7, 17, 74, 21, 7, 9, 26, 10};
-      PartitionEvenOdd(partition_array);
+  int partition_array[] = {7, 17, 74, 21, 7, 9, 26, 10};
+  PartitionEvenOdd(partition_array);
 
-      return 0;
+  return 0;
+  
 }
